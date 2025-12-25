b72070589d4b8d282aea9118049285c567185fe4
Will F <11763006+w4ester@users.noreply.github.com>
2025-12-25 02:03:14 -0500

    Debug env vars - w4ester

--- api/test.js
+++ api/test.js
@@ -1,7 +1,14 @@
 export const config = { runtime: 'edge' };
 
 export default function handler(request) {
-  return new Response(JSON.stringify({ ok: true, time: Date.now() }), {
+  const hasRedis = !!(process.env.UPSTASH_REDIS_REST_URL && process.env.UPSTASH_REDIS_REST_TOKEN);
+  return new Response(JSON.stringify({
+    ok: true,
+    time: Date.now(),
+    redisConfigured: hasRedis,
+    urlSet: !!process.env.UPSTASH_REDIS_REST_URL,
+    tokenSet: !!process.env.UPSTASH_REDIS_REST_TOKEN
+  }), {
     headers: { 'Content-Type': 'application/json' },
   });
 }
